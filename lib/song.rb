30f481582a04644dc6014ecbb75c2d05548d035c
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-20 07:57:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   def self.new_by_filename(file_name)
     artist = Artist.new(file_name.split(" - ")[0])
     song = self.new(file_name.split(" - ")[1])
-    song.artist = artist 
+
     # artist = Artist.new(file_name.split(" - ")[0])
     # song.artist = artist.name 
     # artist.songs << song.name

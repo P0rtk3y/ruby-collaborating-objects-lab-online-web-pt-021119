c3e3565be1db5b5a2f8d881728f9386a8b861b83
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-20 06:27:55 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :artist 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def self.new_by_filename(file_name)
+    new_song = self.new(file_name.split(" - ")[1])       
+    artist = Artist.find_or_create_by_name(file_name.split(" - ")[0])
+  end 
+end  nnnb
